0c1e0e6f3a3b2ad0e5d7ba33408caaf702366d02
mayta4590 <mayta4590@gmail.com>
2026-06-10 16:32:36 -0300

    Delete Act 19 - Oscar Mayta directory

--- Act 19 - Oscar Mayta/punto 3/script.py
+++
@@ -1,5 +0,0 @@
-"""
-3. Confeccionar una función que reciba entre 2 y 5 enteros. La misma nos
-debe retornar la suma de dichos valores. Debe tener tres parámetros por
-defecto.
-"""
--- Act 19 - Oscar Mayta/punto 4/script.py
+++
@@ -1,8 +0,0 @@
-"""
-4. Elaborar una función que muestre la tabla de multiplicar del valor que le
-enviemos como parámetro. Definir un segundo parámetro llamado termino
-que por defecto almacene el valor 10. Se deben mostrar tantos términos de
-la tabla de multiplicar como lo indica el segundo parámetro.
-Llamar a la función desde el bloque principal de nuestro programa con
-argumentos nombrados.
-"""
